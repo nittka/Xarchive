e5d29772ada59d0fa404a7c44fd6e69b14ac82f0
Alexander Nittka <alex@nittka.de>
2016-12-07 21:50:44 +0100

    allow folding of long category and search definitions

--- de.nittka.tooling.xarchive.ui/src/de/nittka/tooling/xarchive/ui/folding/XarchiveFoldingRegionProvider.java
+++ de.nittka.tooling.xarchive.ui/src/de/nittka/tooling/xarchive/ui/folding/XarchiveFoldingRegionProvider.java
@@ -5,16 +5,26 @@ import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.ui.editor.folding.DefaultFoldingRegionProvider;
 
+import de.nittka.tooling.xarchive.xarchive.CategoryType;
 import de.nittka.tooling.xarchive.xarchive.FullTextForSearch;
+import de.nittka.tooling.xarchive.xarchive.Search;
 
 public class XarchiveFoldingRegionProvider extends DefaultFoldingRegionProvider {
 
 	@Override
 	protected boolean isHandled(EObject eObject) {
 		if(eObject instanceof FullTextForSearch){
-			ICompositeNode node = NodeModelUtils.findActualNodeFor(eObject);
-			return node.getEndLine()-node.getStartLine()>2;
+			return numberOfLines(eObject)>2;
+		} else if(eObject instanceof CategoryType){
+			return numberOfLines(eObject)>5;
+		} else if(eObject instanceof Search){
+			return numberOfLines(eObject)>5;
 		}
 		return false;
+	}
+
+	private int numberOfLines(EObject e){
+		ICompositeNode node = NodeModelUtils.findActualNodeFor(e);
+		return node.getEndLine()-node.getStartLine();
 	}
 }
